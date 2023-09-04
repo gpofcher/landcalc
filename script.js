25826d081e93ef518bca4ea4f03f47d5663a3f9c
gpofcher <33699610+gpofcher@users.noreply.github.com>
2023-09-04 10:37:21 -0400

    calculate average mana value

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 console.log("hello world")
 
+let mv_sum = 0;
+let card_count = 0;
+
 function storeManaValues() {
     const numInputs = 16; // Number of input fields
-    const manaValues = [];
-  
+
+
     for (let i = 1; i <= numInputs; i++) {
-      const inputName = `mv${i}`;
-      const inputValue = parseInt(document.querySelector(`[id="${inputName}"]`).value);
+      const inputID = `mv${i}`;
+      const inputValue = parseInt(document.querySelector(`[id="${inputID}"]`).value);
       
       // Check if the inputValue is a valid number (not NaN)
       if (!isNaN(inputValue)) {
-        manaValues.push(inputValue);
+        card_count = card_count + inputValue;
+        mv_sum = mv_sum + (inputValue * i);
+
+        console.log("mv" + i + " card count:" + card_count)
+        console.log(card_count * i)
+        console.log(mv_sum)
       }
     }
-  
-    // Now, manaValues contains the values from the input fields
-    console.log(manaValues);
-  
-    // You can use the manaValues array for further processing as needed
 }
 
 function calcAvg() {
     storeManaValues()
+
+    avgMV = mv_sum / card_count;
+    console.log(avgMV)
+    document.getElementById('avgManaValue').value = avgMV
+
+    mv_sum = 0;
+    card_count = 0;
 }
 
