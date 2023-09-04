42623b35977db1a36f9e3da91ad76dd62e61936c
gpofcher <33699610+gpofcher@users.noreply.github.com>
2023-09-04 10:55:31 -0400

    MVP complete!

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 console.log("hello world")
 
-let mv_sum = 0;
-let card_count = 0;
+// function storeManaValues() {
+//     const numInputs = 16; // Number of input fields
 
-function storeManaValues() {
+
+//     for (let i = 1; i <= numInputs; i++) {
+//       const inputID = `mv${i}`;
+//       const inputValue = parseInt(document.querySelector(`[id="${inputID}"]`).value);
+      
+//       // Check if the inputValue is a valid number (not NaN)
+//       if (!isNaN(inputValue)) {
+//         card_count = card_count + inputValue;
+//         mv_sum = mv_sum + (inputValue * i);
+
+//         console.log("mv" + i + " card count:" + card_count)
+//         console.log(card_count * i)
+//         console.log(mv_sum)
+//       }
+//     }
+// }
+
+function calcAvg() {
     const numInputs = 16; // Number of input fields
-
+    let mv_sum = 0;
+    let card_count = 0;
 
     for (let i = 1; i <= numInputs; i++) {
       const inputID = `mv${i}`;
@@ -21,10 +39,6 @@ function storeManaValues() {
         console.log(mv_sum)
       }
     }
-}
-
-function calcAvg() {
-    storeManaValues()
 
     avgMV = mv_sum / card_count;
     console.log(avgMV)
@@ -34,3 +48,12 @@ function calcAvg() {
     card_count = 0;
 }
 
+function calcLands() {
+    decksize = document.getElementById('decksize').value
+    cheapDraw = document.getElementById('cheapDraw').value
+    avgMV = document.getElementById('avgManaValue').value
+
+    landcount = (decksize / 60) * (19.59 + 1.90 * avgMV) - 0.28 * cheapDraw
+    console.log(landcount)
+    document.getElementById("landcount").innerText = landcount
+}
